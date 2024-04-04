bdf531fcc8a8dbe0f323fa7dcbbb8ab6436127f4
Aamir Yatali <aamir.yatali@my.uwi.edu>
2024-04-04 02:44:51 +0000

    updated auth.js and data.js

--- public/auth.js
+++ public/auth.js
@@ -1,1 +1,79 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.10.0/firebase-app.js";
+import { 
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut, 
+  signInAnonymously, 
+  setPersistence, 
+  browserLocalPersistence, 
+  onAuthStateChanged 
+} from "https://www.gstatic.com/firebasejs/10.10.0/firebase-auth.js";
+import firebaseConfig from "./firebaseConfig.js";
 
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const user = auth.currentUser;
+
+function setAuthListeners(onLogin, onLogout){
+  onAuthStateChanged(auth, user => {
+    if (user) {
+      const uid = user.id;
+      onLogin();
+    } else {
+      onLogout();
+    }
+  });
+}
+
+async function createUser(){
+/*
+  try{
+    await setPersistence(auth, browserLocalPersistence);
+    const user = await signInAnonymously(auth);
+  }catch(e){
+    console.error(e);
+  }
+*/
+  createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed up 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+}
+
+async function signIn(){
+    signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+}
+
+async function logout() {
+ /* try {
+    await signOut(auth);
+  } catch (error) {
+    console.error('Error signing out', error);
+  }
+*/
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        }).catch((error) => {
+        // An error happened.
+    });
+}
+
+export {auth, createUser, setAuthListeners, signIn, logout};
--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,1 +1,15 @@
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCouuLDgiI6ubkuZ2sKu1NgJPu2AhwZhi0",
+  authDomain: "info-1601-project-bdcdc.firebaseapp.com",
+  projectId: "info-1601-project-bdcdc",
+  storageBucket: "info-1601-project-bdcdc.appspot.com",
+  messagingSenderId: "687695748046",
+  appId: "1:687695748046:web:cbe2a564d63aaf6f4c699f"
+};
+
+// Initialize Firebase
+export default firebaseConfig
